71fa78f4ac3f32bfa98cde12130113b34617d79c
FrancescoSilvi <fra.silvi1998@outlook.it>
2019-10-01 16:42:56 +0200

    Create SpringBoot

---
+++ demoSpringBootApp/src/main/java/it/univpm/demoSpringBootApp/DemoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.demoSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootAppApplication.class, args);
+	}
+
+}
