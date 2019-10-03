759d5c2159873d2a5c37606c23d639db4997f145
FrancescoSilvi <fra.silvi1998@outlook.it>
2019-10-03 11:46:33 +0200

    Creazione prima classe

---
+++ demoSpringBootApp/src/main/java/it/univpm/demoSpringBootApp/controller/simpleRestController.java
@@ -0,0 +1,9 @@
+package it.univpm.demoSpringBootApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class simpleRestController {
+	@GetMapping("hello")
+	public HelloWorldClass exampleMethod
+}
