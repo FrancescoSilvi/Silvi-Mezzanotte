45d2cce841f4791335e61b5ec27219e38abd1ba8
FrancescoSilvi <fra.silvi1998@outlook.it>
2019-10-09 15:16:49 +0200

    Modifica filtri (09-10-2019 / 15:16)

--- demoSpringBootApp/src/main/java/Filters/Filters.java
+++ demoSpringBootApp/src/main/java/Filters/Filters.java
@@ -1,16 +1,55 @@
 package Filters;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import it.univpm.ProgettoOOP.data.SavedData;
 import it.univpm.ProgettoOOP.model.FisherAid;
 
 public class Filters {
 ArrayList<FisherAid> AiutiPesca;
+
 public Filters(ArrayList<FisherAid> AiutiPesca) {
 	this.AiutiPesca=AiutiPesca;
+		
 }
-public void check(String val1, String operator, String val2) { // per ora è void, poi si vedrà
+
+public static boolean check (String val, String operator, String CampoRic) {
+	
+	return true;
+	//return false;
+}
+
+public ArrayList<FisherAid> selected(ArrayList<FisherAid> selec, String val, String operator, String CampoRic){
+	ArrayList<FisherAid> out = new ArrayList<FisherAid>();
+	for(FisherAid indice : AiutiPesca) {
+		try {
+			Method m = indice.getClass().getMethod("get"+CampoRic.substring(0,1).toUpperCase()+CampoRic.substring(1), null);
+			try {
+				Object tmp = m.invoke(indice);
+				if (Filters.check(val, operator, CampoRic))
+					out.add(indice);
+			}catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		}
+		return out;
+		}
+
+			
+}
+/*public void check(String val1, String operator, String val2) { // per ora è void, poi si vedrà
 	switch(val2) {
 	case "GEO":
 		if(operator=="nin") {
@@ -25,7 +64,10 @@ public void check(String val1, String operator, String val2) { // per ora è voi
 		}
 		break;
 	case "2000":
-		
+		Method m = AiutiPesca.getClass().getMethod("get"+AiutiPesca.substring(0, 1).toUpperCase()+fieldName.substring(1),null)
+		/*fare un metodo che dica al programma il get da usare in automatico. fare con method (reflect) 
+		 * per esempio: SavedData.getArrFisherAid().get(i).method...ecc*/
+	/*
 		break;
 	case "2001":
 		
@@ -79,6 +121,6 @@ public void check(String val1, String operator, String val2) { // per ora è voi
 		
 		break;
 	
-	}
-}
-}
+	}*/
+
+
