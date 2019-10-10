f6a29048f50838335df3f2600501ffde759bec0a
FrancescoSilvi <fra.silvi1998@outlook.it>
2019-10-10 11:44:23 +0200

    11/44

--- demoSpringBootApp/src/main/java/Filters/Filters.java
+++ demoSpringBootApp/src/main/java/Filters/Filters.java
@@ -17,16 +17,16 @@ public Filters(ArrayList<FisherAid> AiutiPesca) {
 }
 
 public static boolean check (Object ValRif, String operator, Object val) {
-	if(val instanceof String && ValRif instanceof String) {
+	if(val instanceof String && ValRif instanceof String) {	//per le stringhe
 		switch (operator) {
-		case "in":	//>
+		case "in":
 			if ((String)val==(String)ValRif) return true;
 			return false;
 		case "nin":
 			if ((String)val!=(String)ValRif) return true;
 			return false;
 		}
-	}else if(val instanceof Number && ValRif instanceof Number) {
+	}else if(val instanceof Number && ValRif instanceof Number) {	//per i numeri
 		switch (operator) {
 		case "gt":	//>
 			if ((float)val>(float)ValRif) return true;
@@ -43,7 +43,7 @@ public static boolean check (Object ValRif, String operator, Object val) {
 		case "in":	//==
 			if((float)val==(float)ValRif) return true;
 			return false;
-		case "nin":	//==
+		case "nin":	//!=
 			if((float)val!=(float)ValRif) return true;
 			return false;
 	//	case "bt":	//=<value>=
@@ -81,78 +81,3 @@ public ArrayList<FisherAid> selected(ArrayList<FisherAid> selec, String CampoRic
 
 			
 }
-/*public void check(String val1, String operator, String val2) { // per ora è void, poi si vedrà
-	switch(val2) {
-	case "GEO":
-		if(operator=="nin") {
-			for(int i=0; i<SavedData.getArrFisherAid().size();i++) {
-				if(SavedData.getArrFisherAid().get(i).getGeo()!=val1);  //stampare in JSON
-			}
-		}
-		else if(operator=="in") {
-			for(int i=0; i<SavedData.getArrFisherAid().size();i++) {
-				if(SavedData.getArrFisherAid().get(i).getGeo()==val1);  //stampare in JSON
-			}
-		}
-		break;
-	case "2000":
-		Method m = AiutiPesca.getClass().getMethod("get"+AiutiPesca.substring(0, 1).toUpperCase()+fieldName.substring(1),null)
-		/*fare un metodo che dica al programma il get da usare in automatico. fare con method (reflect) 
-		 * per esempio: SavedData.getArrFisherAid().get(i).method...ecc*/
-	/*
-		break;
-	case "2001":
-		
-		break;
-	case "2002":
-		
-		break;
-	case "2003":
-		
-		break;
-	case "2004":
-		
-		break;
-	case "2005":
-		
-		break;
-	case "2006":
-		
-		break;
-	case "2007":
-		
-		break;
-	case "2008":
-		
-		break;
-	case "2009":
-		
-		break;
-	case "2010":
-		
-		break;
-	case "2011":
-		
-		break;
-	case "2012":
-		
-		break;
-	case "2013":
-		
-		break;
-	case "2014":
-		
-		break;
-	case "2015":
-		
-		break;
-	case "2016":
-		
-		break;
-	case "2017":
-		
-		break;
-	
-	}*/
-
-
